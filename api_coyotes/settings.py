2400475900da4a84535ebf26104e1ada519b5781
MarcoEspinozaa <marcoo.espinozaa@gmail.com>
2022-06-24 20:32:08 -0400

    commit2

--- api_coyotes/settings.py
+++ api_coyotes/settings.py
@@ -77,6 +77,13 @@ WSGI_APPLICATION = 'api_coyotes.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'api_coyote',
         'USER': 'postgres',
@@ -85,7 +92,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
